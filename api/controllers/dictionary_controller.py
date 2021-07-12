19688bfef687f9088429c8cb7c596ac16bdf33b4
Nutthanich Narphromar <monaprom134@gmail.com>
2021-07-12 12:07:16 +0700

    Edit controllers to return json and add an example controller

--- api/controllers/dictionary_controller.py
+++ api/controllers/dictionary_controller.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
+from flask import jsonify
 
 dictionary = Blueprint('dictionary', __name__)
 
 
 @dictionary.route('/', methods=['GET'])
 def dictionary_index():
-    return 'Welcome to thsltrans English-SignGloss dictionary'
+    return {
+        'message': 'Welcome to thsltrans English-SignGloss dictionary',
+        'data': None
+    }
--- api/controllers/translator_controller.py
+++ api/controllers/translator_controller.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, abort
+from flask import jsonify
 
 translator = Blueprint('translator', __name__)
 
 
 @translator.route('/', methods=['GET'])
 def translator_index():
-    return 'Welcome to thsltrans rule-based system'
+    # TODO: send json as response
+    return jsonify({
+        'message': 'Welcome to thsltrans rule-based system',
+        'data': None
+    })
+
+
+@translator.route('/404', methods=['GET'])
+def example_404():
+    return jsonify({
+        'message': 'There is no such a thing here'
+    }), 404
