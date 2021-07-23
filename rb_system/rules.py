43b43cec9128a36a69aae22a27af90060c2f8108
Nutthanich Narphromar <monaprom134@gmail.com>
2021-07-23 11:17:45 +0700

    Initialize empty functions

---
+++ rb_system/rules.py
@@ -0,0 +1,6 @@
+def rule_1(text: str):
+    """Rearrange the input text according to the grammar rule #1"""
+    pass
+
+
+# TODO: define the rules for all types of sentence
---
+++ rb_system/translation.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def apply_rules(text: str) -> List:
+    """Return a list of rearranged english words"""
+    pass
+
+
+def map_english_to_sign_gloss(words: List) -> List:
+    """Convert a list of english words to a list of sign glosses"""
+    pass
