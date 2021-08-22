f13b65140779fe65dcf357a0a89f5ae9fe184dcb
Nutthanich Narphromar <monaprom134@gmail.com>
2021-08-22 12:55:24 +0700

    Add a function to perform NLP and add an example code for NLP

--- rb_system/nlp_tools.py
+++ rb_system/nlp_tools.py
@@ -7,19 +7,33 @@ from api.models import TextData
 token.tag_ --> ptb tag set, see: https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 token.pos_ --> Universal POS tags, see: https://universaldependencies.org/u/pos/
 """
-# TODO: consider using in-mem cache when matching doc with sign gloss
+# TODO: consider using in-memory cache when matching doc with sign gloss
+
+nlp: Language = spacy.load('en_core_web_sm')
 
 
-def perform_nlp_process(text_data: TextData) -> Doc:
+def perform_nlp_process(text_data: TextData):
     """
     Perform necessary NLP such as part-of-speech tagging and dependency parsing.
     """
-    pass
+    # Split paragraph into a list of sentences
+    for paragraph in text_data.original:
+        p_doc: Doc = nlp(paragraph)
+        sentence = list(p_doc.sents)
+        text_data.processed_data.append(sentence)
+
+
+def contain_transitive_verb(sentence: str) -> bool:
+    # a children node of a verb contains dobj
+    # https://stackoverflow.com/questions/49271730/how-to-parse-verbs-using-spacy
+    return False
 
 
 if __name__ == '__main__':
-    nlp: Language = spacy.load('en_core_web_sm')
-
+    """
+    Please run the following command if it's the first time you run this module.
+    `python -m spacy download en_core_web_sm`
+    """
     while True:
         text = input('text: ')
         if text == '':
@@ -27,9 +41,18 @@ if __name__ == '__main__':
         doc: Doc = nlp(text)
         words = [token.lemma_ for token in doc]
         print()
-        print(words)
 
-        token: Token
-        for token in doc:
-            print(f'{token.lemma_:<10}{token.pos_:<7}{token.tag_:<7}{token.dep_}')
+        # Sentence detection
+        sentences = list(doc.sents)
+        for s in sentences:
+            print(s)
+            token: Token
+            for token in s:
+                if not token.is_punct:
+                    print(f'{token.lemma_:<10}{token.pos_:<7}{token.tag_:<5}{token.dep_:<10}{spacy.explain(token.dep_)}')
+
+            print(f'Noun phrase: {", ".join([str(n) for n in s.noun_chunks])}')
+            print()
         print('----------------------------')
+
+# dobj: A direct object is a noun phrase that is the accusative object of a (di)transitive verb
