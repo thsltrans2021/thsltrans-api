312f18fe0e42f284d75d9bd7b102c475a5a8c3a4
Nutthanich Narphromar <monaprom134@gmail.com>
2021-08-22 12:57:24 +0700

    Add some code to complete the translation process for now

--- rb_system/rules.py
+++ rb_system/rules.py
@@ -1,9 +1,26 @@
 from typing import List
+from spacy.tokens import Span, Token
+
+"""
+Symbols from the ThSL research
+(+|-)   optional
+(+)     required
+"""
 
 
-def rule_1(text: str) -> List[str]:
-    """Rearrange the input text according to the grammar rule #1"""
-    return ['Rule 1', text]
+def br1_transitive_sentence(text: Span) -> List[str]:
+    """
+    Rearrange the input text according to the grammar rule #1 (p.80)
+    of a basic sentence.
+
+    Type 1:
+        (1) ST = (+|-)S (+|-)O (+)V
+        (2) ST = (+|-)S (+)V (+|-)O
+
+    Type 2:
+        ST = (+|-)S (+)[S - V - O]
+    """
+    return ['Rule 1', str(text)]
 
 
 # TODO: define the rules for all types of sentence
--- rb_system/translation.py
+++ rb_system/translation.py
@@ -1,27 +1,38 @@
 from typing import List
-from rb_system.rules import rule_1
+from rb_system.rules import br1_transitive_sentence
+from rb_system.nlp_tools import perform_nlp_process
 from api.models import TextData
+from spacy.tokens import Span
 
 import logging
 
+"""
+The translation functions that work on a sentence level only
+"""
 
-def apply_rules(text: str) -> List[str]:
+
+def apply_rules(text: Span) -> List[str]:
     """Return a list of rearranged english words"""
-    words = rule_1(text)
+    words = br1_transitive_sentence(text)
+    # can we apply a pattern of the rule to categorize a sentence
     sign_glosses = map_english_to_sign_gloss(words)
     return sign_glosses
 
 
 def map_english_to_sign_gloss(words: List[str]) -> List[str]:
-    """Convert a list of english words to a list of sign glosses"""
+    """
+    Convert a list of english words to a list of sign glosses.
+    Map english words to the ThSL database.
+    """
     return words
 
 
 def translate_english_to_sign_gloss(text_data: TextData) -> List[List[str]]:
+    perform_nlp_process(text_data)
     results = []
-    for idx, paragraph in enumerate(text_data.original):
-        # TODO: perform NLP
+    for idx, paragraph in enumerate(text_data.processed_data):
         logging.info(f'Translating paragraph {idx + 1}')
-        results.append(apply_rules(paragraph))
-    text_data.translation = results
+        for sentence in paragraph:
+            results.append(apply_rules(sentence))
+    text_data.thsl_translation = results
     return results
